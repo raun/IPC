09d62763cd9ba68088a5decc226971908fd7ba8d
Anshul Sharma <anshul@localhost.localdomain>
2014-04-18 22:26:53 +0530

    Corrected the mistakes of turn

--- Two Player Game/player1.c
+++ Two Player Game/player1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include <sys/types.h>
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
@@ -19,35 +19,35 @@ int main(){
 		printf("Error in shmat()\n");
 		exit(2);
 	}
-
+	srand(time(0));
 	*shm_state=0;
 	while(1){
 		while(*shm_state>2){
 			printf("\nWaiting for player 2\n");
-			sleep(10);
+			sleep(2);
 			if(*shm_state==7){
 				shmdt(shm_state);
 				shmctl(shmid, IPC_RMID, 0);
 				exit(0);
 			}
 		}
-		do{
-			diceScore=(rand()%6)+1;
-			printf("\nDice Score : %d Score is:%d",diceScore,score);
-			score=score + diceScore;
-		}while(diceScore!=6);
-		printf("\nSecondary calling...");
-		*shm_state=1;
-		sec = (rand()%100)+1;
-		score= score+sec;
-		printf("\nScore after secondary:%d",score);
-		printf("\nChecking if player 1 won or not??");
-		*shm_state=2;
-		if(score>500){
-			printf("\nPlayer 1 won...");
-			*shm_state=7;
-			shmdt (shm_state);
-			exit(0);
+		diceScore=(rand()%6)+1;
+		printf("\nPlayer 1 dice : %d",diceScore);
+		if(diceScore==6){
+			printf("\nSecondary calling...");
+			*shm_state=1;
+			sec = (rand()%100)+1;
+			printf("\nNumber generated : %d",sec);
+			score= score+sec;
+			printf("\nScore after secondary:%d",score);
+			printf("\nChecking if player 1 won or not??");
+			*shm_state=2;
+			if(score>500){
+				printf("\nPlayer 1 won...");
+				*shm_state=7;
+				shmdt (shm_state);
+				exit(0);
+			}
 		}
 		*shm_state=3;
 	}
--- Two Player Game/player2.c
+++ Two Player Game/player2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include <sys/types.h>
 #include <sys/ipc.h> 
 #include <sys/shm.h> 
@@ -19,12 +19,12 @@ int main(){
 		printf("Error in shmat()\n");
 		exit(2);
 	}
-
+	srand(time(0));
 	*shm_state=0;
 	while(1){
 		while((*shm_state>=0)&&(*shm_state<3)){
 			printf("\nWaiting for player 1 \n");
-			sleep(10);
+			sleep(2);
 			if(*shm_state==7){
 				shmdt(shm_state);
 				shmctl(shmid, IPC_RMID, 0);
@@ -36,22 +36,22 @@ int main(){
 			shmctl(shmid, IPC_RMID, 0);
 			exit(0);
 		}
-		do{
-			diceScore=(rand()%6)+1;
-			printf("\nDice Score : %d Score:%d",diceScore,score);
-			score=score + diceScore;
-		}while(diceScore!=6);
-		printf("\nSecondary calling...");
-		*shm_state=4;
-		sec = (rand()%100)+1;
-		score= score+sec;
-		printf("\nScore after secondary:%d",score);
-		printf("\nChecking if player 2 won or not??");
-		*shm_state=5;
-		if(score>500){
-			printf("Player 2 won...");
-			shmdt(shm_state);
-			exit(0);
+		diceScore=(rand()%6)+1;
+		printf("\nPlayer 2 dice : %d",diceScore);
+		if(diceScore==6){
+			printf("\nSecondary calling...");
+			*shm_state=4;
+			sec = (rand()%100)+1;
+			printf("\nNumber generated : %d",sec);
+			score= score+sec;
+			printf("\nScore after secondary:%d",score);
+			printf("\nChecking if player 2 won or not??");
+			*shm_state=5;
+			if(score>500){
+				printf("Player 2 won...");
+				shmdt(shm_state);
+				exit(0);
+			}
 		}
 		*shm_state=0;
 	}
